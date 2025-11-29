a142bdbbe03a0f185f0a036f680234bddea851de
kevlarwarlock-lab <kevlarwarlock@gmail.com>
2025-11-29 06:39:36 -0800

    bs stuff basically always.

--- source/app.d
+++ source/app.d
@@ -1,68 +1,38 @@
 module app;
 
-import core.time;
+struct EFI_TABLE_HEADER {
+	ulong a;
+	uint b, c, d, e;
 
-import std.stdio;
-import std.parallelism;
-
-import gtk.Bin;
-import gtk.MainWindow;
-import gtk.Label;
-import gtk.Main;
-import gtk.Button;
-import gtk.IconSize;
-
-__gshared bool running = true;
-
-auto getTime() {
-
-	auto now = MonoTime.currTime();
-	auto ticksPer = now.ticksPerSecond();
-
-	auto micros = now.ticks * 1_000_000 / ticksPer;
-
-	return micros;
 }
 
-void render(double iv){}
+struct EFI_SYSTEM_TABLE {
+	EFI_TABLE_HEADER hdr;
 
-void main(string[] args) {
-	Main.initMultiThread(args);
-	MainWindow win = new MainWindow("Hello World");
-	win.setDefaultSize(200, 100);
-	//win.add(new Label("Hello World"));
-	auto btn = new Button("press me", GtkIconSize.BUTTON);
-	btn.setAlignment(32, 32);
-	win.add(new Button("press *this*!"));
-	win.showAll();
-	auto tsk = new Task!(() {
-		enum tps = 1;
-		enum maxSkip = 5;
-		enum skipTicks = 1_000_000 / tps;
-		auto nextTick = getTime();
-		double interpolation=1.0;
-		while (.running) {
-			//writeln("hello");
-			auto loops = 0;
+	wchar* FirmwareVendor;
+	uint FirmwareRevision;
+	void* ConsoleInHandle;
+	void* ConIn;
+	void* ConsoleOutHandle;
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut;
+	void* StandardErrorHandle;
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* StdErr;
 
-			while (getTime() > nextTick && loops < maxSkip) {
-				writefln("hi: %.52f", interpolation);
-				nextTick += skipTicks;
-				
-				loops += 1;
-			}
-
-			auto interp = double(getTime() + skipTicks - nextTick) / double(skipTicks);
-			interpolation = interp;
-
-			render(interp);
-		}
-	});
-	tsk.executeInNewThread();
-	Main.run();
-
-	
-
-	.running = false;
 }
-//0000009999999999177333620536956004798412322998046875
+
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+	void* reset;
+	ulong function(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL*, wchar*) outputString;
+
+}
+
+extern (C):
+void efi_main(void* imageHandle, EFI_SYSTEM_TABLE* systemTable) {
+
+while(true){
+
+	systemTable.ConOut.outputString(systemTable.ConOut,cast(wchar*)"HELLO\n"w.ptr);
+
+}
+//	return;
+}
